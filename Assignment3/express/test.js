cb20a097dba64b83d63ac7dc341baec2c24f0b7d
Snehaa1212 <143368138+Snehaa1212@users.noreply.github.com>
2025-04-25 12:14:29 +0530

    Add files via upload

--- Assignment3/express/test.js
+++ Assignment3/express/test.js
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 const express = require('express')
 const app=express();
 app.use(express.static('public'))
+=======
+const express = require('express')
+const app=express();
+app.use(express.static('public'))
+>>>>>>> 7b76ccd (initial commit)
 app.listen(3000)
